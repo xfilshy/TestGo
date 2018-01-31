7d5037e68760adce306fef4b96fa9e5861ceeef4
xfilshy <xfilshy@gmail.com>
2018-01-31 09:55:53 +0800

    asdad

--- app/src/main/java/com/xue/adapter/ChatListAdapter.java
+++ app/src/main/java/com/xue/adapter/ChatListAdapter.java
@@ -21,6 +21,18 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.BaseVi
 
     private final int TYPE_TEXT_MSG_LEFT = 2;
 
+    private final int TYPE_AUDIO_MSG_RIGHT = 3;
+
+    private final int TYPE_AUDIO_MSG_LEFT = 4;
+
+    private final int TYPE_VIDEO_MSG_RIGHT = 5;
+
+    private final int TYPE_VIDEO_MSG_LEFT = 6;
+
+    private final int TYPE_AVCHAT_MSG_RIGHT = 7;
+
+    private final int TYPE_AVCHAT_MSG_LEFT = 8;
+
     private List<IMMessage> mIMMessageList;
 
     public ChatListAdapter() {
@@ -39,6 +51,24 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.BaseVi
             } else {
                 return TYPE_TEXT_MSG_LEFT;
             }
+        } else if (imMessage.getMsgType().getValue() == MsgTypeEnum.audio.getValue()) {
+            if (TextUtils.equals(imMessage.getFromAccount(), BaseApplication.get().getUserId())) {
+                return TYPE_AUDIO_MSG_RIGHT;
+            } else {
+                return TYPE_AUDIO_MSG_LEFT;
+            }
+        } else if (imMessage.getMsgType().getValue() == MsgTypeEnum.video.getValue()) {
+            if (TextUtils.equals(imMessage.getFromAccount(), BaseApplication.get().getUserId())) {
+                return TYPE_VIDEO_MSG_RIGHT;
+            } else {
+                return TYPE_VIDEO_MSG_LEFT;
+            }
+        } else if (imMessage.getMsgType().getValue() == MsgTypeEnum.avchat.getValue()) {
+            if (TextUtils.equals(imMessage.getFromAccount(), BaseApplication.get().getUserId())) {
+                return TYPE_AVCHAT_MSG_RIGHT;
+            } else {
+                return TYPE_AVCHAT_MSG_LEFT;
+            }
         }
 
         return 0;
@@ -51,6 +81,18 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.BaseVi
             baseViewHolder = new TextMessageRightViewHolder(parent);
         } else if (viewType == TYPE_TEXT_MSG_LEFT) {
             baseViewHolder = new TextMessageLeftViewHolder(parent);
+        } else if (viewType == TYPE_AUDIO_MSG_RIGHT) {
+            baseViewHolder = new AudioMessageRightViewHolder(parent);
+        } else if (viewType == TYPE_AUDIO_MSG_LEFT) {
+            baseViewHolder = new AudioMessageLeftViewHolder(parent);
+        } else if (viewType == TYPE_VIDEO_MSG_RIGHT) {
+            baseViewHolder = new VideoMessageRightViewHolder(parent);
+        } else if (viewType == TYPE_VIDEO_MSG_LEFT) {
+            baseViewHolder = new VideoMessageLeftViewHolder(parent);
+        } else if (viewType == TYPE_AVCHAT_MSG_RIGHT) {
+            baseViewHolder = new AVChatMessageRightViewHolder(parent);
+        } else if (viewType == TYPE_AVCHAT_MSG_LEFT) {
+            baseViewHolder = new AVChatMessageLeftViewHolder(parent);
         }
         return baseViewHolder;
     }
@@ -115,6 +157,132 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.BaseVi
         @Override
         public void fillData(IMMessage imMessage) {
             message.setText(imMessage.getContent());
+        }
+    }
+
+    public static class AudioMessageLeftViewHolder extends BaseViewHolder {
+
+        private ImageView photo;
+
+        private TextView account;
+
+        public AudioMessageLeftViewHolder(ViewGroup itemView) {
+            super(LayoutInflater.from(itemView.getContext()).inflate(R.layout.item_chat_audio_left, itemView, false));
+            findView();
+        }
+
+        private void findView() {
+            photo = itemView.findViewById(R.id.photo);
+            account = itemView.findViewById(R.id.account);
+        }
+
+        @Override
+        public void fillData(IMMessage imMessage) {
+        }
+    }
+
+    public static class AudioMessageRightViewHolder extends BaseViewHolder {
+
+        private ImageView photo;
+
+        private TextView account;
+
+        public AudioMessageRightViewHolder(ViewGroup itemView) {
+            super(LayoutInflater.from(itemView.getContext()).inflate(R.layout.item_chat_audio_right, itemView, false));
+            findView();
+        }
+
+        private void findView() {
+            photo = itemView.findViewById(R.id.photo);
+            account = itemView.findViewById(R.id.account);
+        }
+
+        @Override
+        public void fillData(IMMessage imMessage) {
+        }
+    }
+
+    public static class VideoMessageLeftViewHolder extends BaseViewHolder {
+
+        private ImageView photo;
+
+        private TextView account;
+
+        public VideoMessageLeftViewHolder(ViewGroup itemView) {
+            super(LayoutInflater.from(itemView.getContext()).inflate(R.layout.item_chat_video_left, itemView, false));
+            findView();
+        }
+
+        private void findView() {
+            photo = itemView.findViewById(R.id.photo);
+            account = itemView.findViewById(R.id.account);
+        }
+
+        @Override
+        public void fillData(IMMessage imMessage) {
+        }
+    }
+
+    public static class VideoMessageRightViewHolder extends BaseViewHolder {
+
+        private ImageView photo;
+
+        private TextView account;
+
+        public VideoMessageRightViewHolder(ViewGroup itemView) {
+            super(LayoutInflater.from(itemView.getContext()).inflate(R.layout.item_chat_video_right, itemView, false));
+            findView();
+        }
+
+        private void findView() {
+            photo = itemView.findViewById(R.id.photo);
+            account = itemView.findViewById(R.id.account);
+        }
+
+        @Override
+        public void fillData(IMMessage imMessage) {
+        }
+    }
+
+    public static class AVChatMessageLeftViewHolder extends BaseViewHolder {
+
+        private ImageView photo;
+
+        private TextView account;
+
+        public AVChatMessageLeftViewHolder(ViewGroup itemView) {
+            super(LayoutInflater.from(itemView.getContext()).inflate(R.layout.item_chat_avchat_left, itemView, false));
+            findView();
+        }
+
+        private void findView() {
+            photo = itemView.findViewById(R.id.photo);
+            account = itemView.findViewById(R.id.account);
+        }
+
+        @Override
+        public void fillData(IMMessage imMessage) {
+        }
+    }
+
+    public static class AVChatMessageRightViewHolder extends BaseViewHolder {
+
+        private ImageView photo;
+
+        private TextView account;
+
+        public AVChatMessageRightViewHolder(ViewGroup itemView) {
+            super(LayoutInflater.from(itemView.getContext()).inflate(R.layout.item_chat_avchat_right, itemView, false));
+            findView();
+        }
+
+        private void findView() {
+            photo = itemView.findViewById(R.id.photo);
+            account = itemView.findViewById(R.id.account);
+        }
+
+        @Override
+        public void fillData(IMMessage imMessage) {
         }
     }
 
